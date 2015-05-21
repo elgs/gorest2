7784eee3cae998cd0c14c6713502efaeb0c45a38
elgs <elgs1980@hotmail.com>
2015-05-22 00:29:26 +0800

    Update.

--- handler.go
+++ handler.go
@@ -2,6 +2,8 @@
 package gorest2
 
 import (
+	"fmt"
+	"github.com/elgs/gosqljson"
 	"net/http"
 )
 
@@ -22,5 +24,37 @@ func RegisterDbo(id string, dbo DataOperator) {
 }
 
 func GetDbo(id string) DataOperator {
-	return dboRegistry[id]
+	return LoadDbo(id)
+}
+
+func LoadDbo(id string) DataOperator {
+	ret := dboRegistry[id]
+	if ret != nil {
+		return ret
+	}
+	defaultDbo := GetDbo("default")
+	db, err := defaultDbo.GetConn()
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	query := `SELECT data_store.* FROM project
+		INNER JOIN data_store ON project.DATA_STORE_NAME = data_store.DATA_STORE_KEY
+		WHERE project.PROJECT_KEY=?`
+	data, err := gosqljson.QueryDbToMap(db, query, id)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	if data == nil || len(data) == 0 {
+		return nil
+	}
+	dboData := data[0]
+	ret = &MySqlDataOperator{
+		Ds: fmt.Sprintf("%v:%v@tcp(%v:%v)/%v", dboData["USERNAME"], dboData["PASSWORD"],
+			dboData["HOST"], dboData["PORT"], dboData["DB"]),
+		DbType: "mysql",
+	}
+	RegisterDbo(id, ret)
+	return ret
 }
