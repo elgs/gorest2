f90cfcd23a02ac965471914ee1e4a45e6d38a0b3
elgs <elgs1980@hotmail.com>
2015-05-24 04:08:02 +0800

    Update.

--- handler.go
+++ handler.go
@@ -2,8 +2,6 @@
 package gorest2
 
 import (
-	"fmt"
-	"github.com/elgs/gosqljson"
 	"net/http"
 )
 
@@ -17,44 +15,8 @@ func GetHandler(id string) func(w http.ResponseWriter, r *http.Request) {
 	return handlerRegistry[id]
 }
 
-var dboRegistry = make(map[string]DataOperator)
+var DboRegistry = make(map[string]DataOperator)
 
-func RegisterDbo(id string, dbo DataOperator) {
-	dboRegistry[id] = dbo
-}
-
-func GetDbo(id string) DataOperator {
-	return LoadDbo(id)
-}
-
-func LoadDbo(id string) DataOperator {
-	ret := dboRegistry[id]
-	if ret != nil {
-		return ret
-	}
-	defaultDbo := GetDbo("default")
-	db, err := defaultDbo.GetConn()
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
-	query := `SELECT data_store.* FROM project
-		INNER JOIN data_store ON project.DATA_STORE_NAME = data_store.DATA_STORE_KEY
-		WHERE project.PROJECT_KEY=?`
-	data, err := gosqljson.QueryDbToMap(db, query, id)
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
-	if data == nil || len(data) == 0 {
-		return nil
-	}
-	dboData := data[0]
-	ret = &MySqlDataOperator{
-		Ds: fmt.Sprintf("%v:%v@tcp(%v:%v)/%v", dboData["USERNAME"], dboData["PASSWORD"],
-			dboData["HOST"], dboData["PORT"], dboData["DB"]),
-		DbType: "mysql",
-	}
-	RegisterDbo(id, ret)
-	return ret
+var GetDbo = func(id string) DataOperator {
+	return DboRegistry[id]
 }
